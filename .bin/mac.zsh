5a26d3514d93eb1a0260d21ce0f04d7a510f3313
kaito tada <kaito.tada@relic.co.jp>
2026-05-08 16:19:14 +0900

    [add] mac.zsh: Android Studio の env 設定を追加
    ANDROID_HOME / emulator / platform-tools のパスと、Android Studio 同梱
    JDK の JAVA_HOME を export。

--- .bin/mac.zsh
+++ .bin/mac.zsh
@@ -1,1 +1,9 @@
 source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+# Android Studioの設定
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+
+# Android Studio の JDK を使用
+export JAVA_HOME="/Applications/Android Studio.app/Contents/jbr/Contents/Home"
